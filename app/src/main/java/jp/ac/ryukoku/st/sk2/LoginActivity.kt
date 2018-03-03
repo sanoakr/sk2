9807227d8149daa17262b5c8dd52b51629155265
SANO Akira <sano@maica.org>
2018-03-04 00:45:43 +0900

    add inputType of Login/Password editText on LoginActivity.

--- app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivity.kt
+++ app/src/main/java/jp/ac/ryukoku/st/sk2/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
 import android.view.Gravity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
@@ -25,19 +27,22 @@ class LoginActivityUi: AnkoComponent<LoginActivity> {
         verticalLayout {
             padding = dip(16)
 
-            textView("全学認証ログイン"){
+            textView("龍大全学認証ログイン"){
                 textSize = 20f
             }.lparams{
                 width = matchParent
                 topMargin = dip(8)
+                bottomMargin = dip(8)
             }
 
             val title = editText {
-                hint = "全学認証ID"
+                hint = "学籍番号"
+                inputType = TYPE_CLASS_TEXT
             }
 
             val description = editText {
                 hint = "パスワード"
+                inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD
             }
 
             button("ログイン") {
