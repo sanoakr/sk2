44a6b13108a8280dbc9fb2bdee6d162bc0dea55b
SANO Akira <sano@maica.org>
2018-03-08 21:45:51 +0900

    add Weekday to history records.

--- app/src/main/java/jp/ac/ryukoku/st/sk2/RecordActivity.kt
+++ app/src/main/java/jp/ac/ryukoku/st/sk2/RecordActivity.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import me.mattak.moment.Moment
 import org.jetbrains.anko.*
 import java.io.*
 import java.net.InetSocketAddress
+import java.util.*
 import javax.net.ssl.SSLSocketFactory
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -65,7 +67,7 @@ class RecordActivity : AppCompatActivity(),AnkoLogger {
                 "ssid1", "bssid1", "signal1", "ssid2", "bssid2", "signal2",
                 "ssid3", "bssid3", "signal3", "ssid4", "bssid4", "signal4")
         var record: MutableList<MutableMap<String, String>> = mutableListOf()
-        info(record)
+
         if (data.isNullOrBlank()) {
             record.add(mutableMapOf("enUid" to "There is no record"))
         } else {
@@ -79,7 +81,31 @@ class RecordActivity : AppCompatActivity(),AnkoLogger {
                     val keySize = keys.size
                     for (i in vList.indices) {
                         if (!(i < keySize)) break
-                        vMap.put(keys[i], vList[i])
+
+                        // add weekday?
+                        if (keys[i] == "datetime") {
+                            var str = vList[i]
+                            try {
+                                val calendar = Calendar.getInstance()
+                                val match = Regex("(\\d+)-(\\d+)-(\\d+)\\s+(\\d+):(\\d+):(\\d+)").find(str)?.groupValues
+                                if (match?.size == 7) { // Null makes false
+                                    val y = match[1].toInt()
+                                    val m = match[2].toInt()
+                                    val d = match[3].toInt()
+                                    //val th =  match[4].toInt()
+                                    //val tm =  match[5].toInt()
+                                    //val ts =  match[6].toInt()
+                                    calendar.set(y, m, d, 0, 0, 0)
+                                    val wday = Moment(calendar.time, TimeZone.getDefault(), Locale.JAPAN).weekdayName
+                                    str = vList[i].replace(" ", " $wday ")
+                                }
+                                else { str = vList[i] }
+                            }
+                            catch(e: Exception) { str = vList[i] }
+
+                            vMap.put(keys[i], str)
+                        }
+                        else { vMap.put(keys[i], vList[i]) }
                     }
                     record.add(vMap)
                 }
@@ -140,6 +166,22 @@ class RecordAdapter(var activity: RecordActivity): BaseAdapter() {
                     }.lparams { horizontalGravity = right }
 
                     textView("(" + item["signal0"] + "dB)") {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = right; leftMargin = dip(5) }
+                }
+
+                linearLayout {
+                    padding = dip(3)
+
+                    textView(item["bssid1"]) {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = left; weight = 1f }
+
+                    textView(item["ssid1"]) {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = right }
+
+                    textView("(" + item["signal1"] + "dB)") {
                         textSize = 14f
                     }.lparams { horizontalGravity = right; leftMargin = dip(5) }
                 }
