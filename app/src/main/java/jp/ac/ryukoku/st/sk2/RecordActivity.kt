71dd7355f0609bc2124c54868643c43f3f3beba1
SANO Akira <sano@maica.org>
2018-03-08 15:53:33 +0900

    add getting record history from sk2info server on RecordActivity.

--- app/src/main/java/jp/ac/ryukoku/st/sk2/RecordActivity.kt
+++ app/src/main/java/jp/ac/ryukoku/st/sk2/RecordActivity.kt
@@ -1,21 +1,22 @@
 package jp.ac.ryukoku.st.sk2
 
 import android.content.Context
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
 import org.jetbrains.anko.*
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.*
 import java.net.InetSocketAddress
 import javax.net.ssl.SSLSocketFactory
 
 ////////////////////////////////////////////////////////////////////////////////
-class RecordActivity : AppCompatActivity() {
+class RecordActivity : AppCompatActivity(),AnkoLogger {
     private lateinit var username: String
     private lateinit var key: String
+    private val prefName = "st.ryukoku.sk2"
 
     ////////////////////////////////////////
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +25,11 @@ class RecordActivity : AppCompatActivity() {
         title = "出席記録：龍大理工学部出欠システム sk2"
         RecordActivityUi().setContentView(this)
 
-        username = getSharedPreferences("ryukoku.attend", Context.MODE_PRIVATE).getString("uid", "")
-        key = getSharedPreferences("ryukoku.attend", Context.MODE_PRIVATE).getString("key", "")
+        username = getSharedPreferences(prefName, Context.MODE_PRIVATE).getString("uid", "")
+        key = getSharedPreferences(prefName, Context.MODE_PRIVATE).getString("key", "")
     }
     ////////////////////////////////////////
-    fun fetchRecord(user: String, key: String): List<Map<String, Any>> {
+    fun fetchRecord(): List<Map<String, String>> {
         // sk2 info server
         val serverHost = "sk2.st.ryukoku.ac.jp"
         val serverPort = 4441
@@ -46,7 +47,7 @@ class RecordActivity : AppCompatActivity() {
             val bufReader = BufferedReader(InputStreamReader(input, "UTF-8"))
             val bufWriter = BufferedWriter(OutputStreamWriter(output, "UTF-8"))
             // Send message
-            val message = "$user,$key"
+            val message = "$username,$key"
             bufWriter.write(message)
             bufWriter.flush()
             // Receive message
@@ -54,55 +55,107 @@ class RecordActivity : AppCompatActivity() {
 
         } catch (e: Exception) {
             toast("サーバーに接続できません")
-            rowRecord = ""
+            rowRecord = ",Server Error: " + e.toString()
         }
         return parseRecord(rowRecord)
     }
     ////////////////////////////////////////
-    fun parseRecord(data: String): List<Map<String, Any>> {
-        var record = mutableListOf(mutableMapOf<String, String>().withDefault{""})
-
+    fun parseRecord(data: String): List<Map<String, String>> {
+        val keys = listOf("encUid", "type", "datetime", "ssid0", "bssid0", "signal0",
+                "ssid1", "bssid1", "signal1", "ssid2", "bssid2", "signal2",
+                "ssid3", "bssid3", "signal3", "ssid4", "bssid4", "signal4")
+        var record: MutableList<MutableMap<String, String>> = mutableListOf()
+        info(record)
         if (data.isNullOrBlank()) {
-            record.add(Map("enUid" to "There is no record"))
+            record.add(mutableMapOf("enUid" to "There is no record"))
         } else {
             val dataList = data.split('\n')
 
-            try {
-                dataList.forEach { r ->
+            dataList.forEach { r ->
+                if (! r.isNullOrEmpty()) {
+                    var vMap = mutableMapOf<String, String>().withDefault { "" }
                     val vList = r.split(',')
-                    var vMap = mutableMapOf<String, String>().withDefault{""}
 
-                    // vList を value として vMap を生成する
+                    val keySize = keys.size
+                    for (i in vList.indices) {
+                        if (!(i < keySize)) break
+                        vMap.put(keys[i], vList[i])
+                    }
+                    record.add(vMap)
                 }
-
-
             }
-
         }
         return record
+    }
 }
-
 ////////////////////////////////////////////////////////////////////////////////
 class RecordActivityUi: AnkoComponent<RecordActivity> {
-    lateinit var recAdapter: RecordAdapter
-
     ////////////////////////////////////////
     override fun createView(ui: AnkoContext<RecordActivity>) = with(ui) {
-        recAdapter = RecordAdapter(owner)
-
         verticalLayout {
-            relativeLayout {
-                textView("出席記録").lparams { centerHorizontally() }
-            }
-        }
+            padding = dip(10)
 
-        listView {
-            adapter = recAdapter
+            listView {
+                doAsync {
+                    val recAdapter = RecordAdapter(ui.owner)
+                    uiThread {
+                        adapter = recAdapter
+                    }
+                }
+            }
         }
     }
 }
-
 ////////////////////////////////////////////////////////////////////////////////
-class RecordAdapter(var contex: Context): BaseAdapter() {
+class RecordAdapter(var activity: RecordActivity): BaseAdapter() {
+    var list : List<Map<String, String>> = activity.fetchRecord()
+    ////////////////////////////////////////
+    override fun getView(i : Int, v : View?, parent : ViewGroup?) : View {
+        val item = getItem(i)
+        return with(parent!!.context) {
+            verticalLayout {
+                linearLayout {
+                    padding = dip(5)
 
+                    textView(item["type"]) {
+                        textSize = 18f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }.lparams { width = matchParent; horizontalGravity = left; weight = 1f }
+
+                    textView(item["datetime"]) {
+                        textSize = 18f
+                        typeface = Typeface.DEFAULT_BOLD
+                    }.lparams { horizontalGravity = right }
+                }.lparams { width = matchParent }
+
+                linearLayout {
+                    padding = dip(3)
+
+                    textView(item["bssid0"]) {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = left; weight = 1f }
+
+                    textView(item["ssid0"]) {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = right }
+
+                    textView("(" + item["signal0"] + "dB)") {
+                        textSize = 14f
+                    }.lparams { horizontalGravity = right; leftMargin = dip(5) }
+                }
+            }
+        }
+    }
+    ////////////////////////////////////////
+    override fun getItem(position : Int) : Map<String, String> {
+        return list.get(position)
+    }
+    ////////////////////////////////////////
+    override fun getCount() : Int {
+        return list.size
+    }
+    ////////////////////////////////////////
+    override fun getItemId(position : Int) : Long {
+        return 0L
+    }
 }
