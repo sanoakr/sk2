ee4171815f60cbc3196cc9dd608c312eaba6f372
SANO AKira <sano@math.ryukoku.ac.jp>
2018-03-12 21:03:41 +0900

    add web html

---
+++ web/search.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php
+       define("dbServer", "localhost")
+       define("dbName", "sk2")
+       define("dbUser", "sk2")
+       define("dbPass", "sk2loglog")
+</html>
